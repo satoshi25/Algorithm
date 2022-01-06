87a6182cc29196677b71a9335aacbd629c0a468b
CodeStates <codestates@CodeStatesui-MacBookPro.local>
2022-01-06 22:58:52 +0900

    Level1 Add Missing Numbers
    레벨1 없는 숫자 더하기

---
+++ 2022-01/0106-Programmers/level1_addMissingNumbers.js
@@ -0,0 +1,25 @@
+// 0부터 9까지의 숫자 중 일부가 들어있는 배열 numbers가 매개변수로 주어집니다. 
+// numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 
+// solution 함수를 완성해주세요.
+
+// https://programmers.co.kr/learn/courses/30/lessons/86051
+
+// 제한사항
+// 1 ≤ numbers의 길이 ≤ 9
+// 0 ≤ numbers의 모든 수 ≤ 9
+// numbers의 모든 수는 서로 다릅니다.
+
+// numbers            result
+// [1,2,3,4,6,7,8,0]	  14
+// [5,8,4,0,6,7,9]	     6
+
+
+function solution(numbers) {
+  let answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;;
+  
+  for (let i = 0; i < numbers.length; i++) {
+      answer -= numbers[i];
+  }
+  
+  return answer;
+}
