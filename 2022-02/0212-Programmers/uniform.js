c402212b9e61bc957d91853cf6a8947f07398eb4
satoshi25 <satoshi25.kr@gmail.com>
2022-02-13 23:53:32 +0900

    Level1 Uniform edit

--- 2022-02/0212-Programmers/uniform.js
+++ 2022-02/0212-Programmers/uniform.js
@@ -2,22 +2,37 @@
 
 // https://programmers.co.kr/learn/courses/30/lessons/42862
 
-// 5, 7, 12, 13, 18
+// 7, 13, 18
 
 function solution(n, lost, reserve) {
-  let answer = n - lost.length;
 
-  for (let i = 0; i < lost.length; i++) {
-      for (let j = 0; j < reserve.length; j++) {
-          let pre = lost[i] - 1;
-          let next = lost[i] + 1;
-          let cur = lost[i]
-          if (pre === reserve[j] | next === reserve[j] | cur === reserve[j]) {
-              reserve.splice(j, 1);
-              answer += 1;
-              continue;
-          }
-      }
-  }
-  return answer;
+    for (let i = 0; i < lost.length; i++) {
+        for (let j = 0; j < reserve.length; j++) {
+            if (lost[i] === reserve[j]) {
+                reserve.splice(j, 1);
+                lost[i] = 'x';
+                continue;
+            }
+        }
+    }
+
+    lost = lost.filter((lostone) => {
+        return lostone !== 'x'
+    })
+    let answer = n - lost.length;
+
+    for (let i = 0; i < lost.length; i++) {
+        for (let j = 0; j < reserve.length; j++) {
+            let pre = lost[i] - 1;
+            let next = lost[i] + 1;
+            let cur = lost[i]
+            if (pre === reserve[j] | next === reserve[j] | cur === reserve[j]) {
+                reserve.splice(j, 1);
+                answer += 1;
+                continue;
+            }
+        }
+    }
+    
+    return answer;
 }
