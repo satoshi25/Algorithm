f86f7cae468adf61977d9683a920098f6da38670
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2023-02-08 23:58:36 +0900

    인덱스 바꾸기

---
+++ 2023-02/0208-Programmers/0208_changeIndex.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+//
+
+function solution(my_string, num1, num2) {
+	var answer = '';
+	let arr = my_string.split('');
+	let temp = arr[num1];
+	arr[num1] = arr[num2];
+	arr[num2] = temp;
+	answer = arr.join('');
+	return answer;
+}
