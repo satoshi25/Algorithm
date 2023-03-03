d33ca821c47f931e80256f79318de9a3b3e74982
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2023-03-03 21:23:51 +0900

    백준 10998

---
+++ 2023-03/0303-Beakjoon/0303_10998.js
@@ -0,0 +1,46 @@
+/*
+A*B
+
+문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.
+
+예제 입력 1 
+1 2
+예제 출력 1 
+2
+
+예제 입력 2 
+3 4
+예제 출력 2 
+12
+*/
+
+function solution(input) {
+  let [a, b] = input;
+  let answer = a * b;
+  console.log(answer);
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+let list = [];
+
+rl.on('line', function (line) {
+  input = line;
+  rl.close();
+}).on('close', function () {
+  list = input.split(' ').map((el) => parseInt(el));
+  solution(list);
+  process.exit();
+});
