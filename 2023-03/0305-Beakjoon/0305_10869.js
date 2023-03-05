795a8b55081a4cb1444239abd7c61aee8c17f903
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2023-03-05 20:38:22 +0900

    백준 10869 사칙연산

---
+++ 2023-03/0305-Beakjoon/0305_10869.js
@@ -0,0 +1,31 @@
+/*
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+출력
+첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+예제 입력 1 
+7 3
+예제 출력 1 
+10
+4
+21
+2
+1
+*/
+
+let fs = require('fs');
+let [a, b] = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .split(' ')
+  .map((el) => Number(el));
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
